4e3965ef460f6ad1e63ddb41621f499d55660660
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-09 17:50:47 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nearfive::Application.config.secret_token = '27fd701a03a3ccbba7a562086740537bc8c483ca14a719555d9f911c7bb3341ed128a77839c4d79ae6d87c9b772f561d2d46929d3278c05f144ce013804d18ff'
