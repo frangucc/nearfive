a95597bebdd45b7985e3c062e360763d445dbe8e
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-10 22:20:48 +0700

    precompile jquery.validate.min

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -45,7 +45,7 @@ Nearfive::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( application.css store/variables.css)
+  config.assets.precompile += %w( application.css store/variables.css jquery.validate.min.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
