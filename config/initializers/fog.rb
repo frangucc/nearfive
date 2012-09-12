4d43775e735a8713f0545bd6f2db54c8127807bc
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-12 16:07:49 +0700

    set cachedir for spree carrierwave

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,6 +3,7 @@ s3_credentials = YAML.load_file(s3_config)[Rails.env]
 
 CarrierWave.configure do |config|
   unless Rails.env.test? || Rails.env.development?
+    config.cache_dir = "#{Rails.root}/tmp/"
     config.storage = :fog
     config.fog_directory = ENV['S3_BUCKET'] || s3_credentials['bucket']
 
