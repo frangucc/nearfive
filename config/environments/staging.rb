cf10e2c36553d0628d532ecb5740415af9483516
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-12 11:27:01 +0700

    move assets host to nearfive.com

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,7 @@ Nearfive::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false 
+  config.assets.compile = true 
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -26,7 +26,7 @@ Nearfive::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.action_mailer.default_url_options = { :host => '198.101.206.122' }
+  config.action_mailer.default_url_options = { :host => 'nearfive.com' }
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -41,7 +41,7 @@ Nearfive::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://198.101.206.122"
+  config.action_controller.asset_host = "http://nearfive.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
