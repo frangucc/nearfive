e7dd133371f64e2704080b0ddd90626d5d55c9f0
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-12 14:13:15 +0700

    s3 config

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,18 @@
+s3_config = "#{Rails.root}/config/amazon_s3.yml"
+s3_credentials = YAML.load_file(s3_config)[Rails.env]
+
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-  config.fog_directory  = 'my-bucket'
-  config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
+  unless Rails.env.test? || Rails.env.development?
+    config.storage = :fog
+    config.fog_directory = ENV['S3_BUCKET'] || s3_credentials['bucket']
+
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_KEY'] || s3_credentials['access_key_id'],
+      :aws_secret_access_key => ENV['S3_SECRET'] || s3_credentials['secret_access_key']
+    }
+	  config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
+  else
+    config.storage = :file
+  end
 end
